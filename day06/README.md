32eaf5bfa41389c673bcf9186aa9311656135282
Tudor Pavel <tudorv.pavel@gmail.com>
2020-12-06 10:44:08 +0200

    Day 6 - Part 2

--- day06/README.md
+++ day06/README.md
@@ -5,3 +5,18 @@ https://adventofcode.com/2020/day/6
 ## Part 1
 
 I'm going to attempt to create Sets of chars to count unique answers in each group, all in a single functional pipeline.
+
+## Part 2
+
+Ah, I should've seen this coming. Basically I think we need to construct bitstrings for each person's answers and use bitwise logical operators OR for Part 1, AND for Part 2.
+
+```
+abc will be:
+
+abcdefghijklmnopqrstuvwxyz
+11100000000000000000000000
+```
+
+And for counting 1s we can also use bitwise shift like [explained here](https://www.techiedelight.com/brian-kernighans-algorithm-count-set-bits-integer/).
+
+I think it will feel a bit uncomfortable, but this might be fun to implement. Let's see how it goes.
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,23 +1,42 @@
-use std::collections::HashSet;
+fn convert_answers(answers: &str) -> u32 {
+    answers
+        .chars()
+        .fold(0, |acc, c| acc + 2_u32.pow(c as u32 - b'a' as u32))
+}
 
-fn solve(lines: Vec<String>) -> usize {
+fn count_1_bits(mut num: u32) -> u32 {
+    let mut count = 0;
+
+    while num != 0 {
+        count += num & 1;
+        num >>= 1;
+    }
+
+    count
+}
+
+fn solve(lines: Vec<String>) -> (u32, u32) {
     lines
         .split(|l| l == "")
-        .map(|vec| vec.join(""))
         .map(|group| {
-            group.chars().fold(HashSet::new(), |mut set, c| {
-                set.insert(c);
-                set
-            })
+            let bitstrings = group.iter().map(|s| convert_answers(&s));
+
+            (
+                bitstrings.clone().fold(0, |acc, bs| acc | bs),
+                bitstrings.fold(2_u32.pow(26) - 1, |acc, bs| acc & bs),
+            )
         })
-        .map(|set| set.len())
-        .sum()
+        .map(|(bs1, bs2)| (count_1_bits(bs1), count_1_bits(bs2)))
+        .fold((0, 0), |(acc1, acc2), (res1, res2)| {
+            (acc1 + res1, acc2 + res2)
+        })
 }
 
 fn main() {
     let lines = utils::read_lines();
-    let sum = solve(lines);
-    println!("Part 1: {}", sum);
+    let (part1, part2) = solve(lines);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 #[cfg(test)]
@@ -33,7 +52,7 @@ mod tests {
                     .map(|s| String::from(s))
                     .collect()
             ),
-            11
+            (11, 6)
         );
     }
 }
