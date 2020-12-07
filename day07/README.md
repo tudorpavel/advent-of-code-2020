c04b1908422a7c143b8e0b842bfd694454aba119
Tudor Pavel <tudorv.pavel@gmail.com>
2020-12-07 10:14:04 +0200

    Day 7 - Part 1

---
+++ day07/README.md
@@ -0,0 +1,11 @@
+# Advent of Code 2020 - Day 7
+
+https://adventofcode.com/2020/day/7
+
+## Part 1
+
+We should be able to model the input data as a [Directed acyclic graph](https://en.wikipedia.org/wiki/Directed_acyclic_graph). Here is the example visualized:
+
+![example_graph](example_graph.png)
+
+Once the graph data structure is in place, we can have a recursive procedure traverse the graph starting from a given vertex and return boolean if `shiny gold` can be found.
---
+++ day07/src/main.rs
@@ -0,0 +1,95 @@
+use regex::Regex;
+use std::collections::HashMap;
+
+fn build_dag(lines: Vec<String>) -> (HashMap<String, Vec<String>>, HashMap<(String, String), u32>) {
+    let mut dag = HashMap::new();
+    let mut edge_costs = HashMap::new();
+    let re_vertex = Regex::new(r"(.+) bags").unwrap();
+    let re_neighbor = Regex::new(r"(\d+) (.+) (bag|bags)").unwrap();
+
+    for line in lines {
+        let pair: Vec<&str> = line.split(" contain ").collect();
+        let cap_vertex = re_vertex.captures(pair[0]).unwrap();
+        let vertex = cap_vertex[1].to_string();
+        dag.insert(vertex.clone(), vec![]);
+
+        if pair[1] == "no other bags." {
+            continue;
+        }
+
+        let adj_list = dag.get_mut(&vertex).unwrap();
+
+        for neighbor in pair[1].split(", ") {
+            let cap = re_neighbor.captures(neighbor).unwrap();
+            let count = cap[1].parse().unwrap();
+            let name = cap[2].to_string();
+
+            adj_list.push(name.clone());
+            edge_costs.insert((vertex.clone(), name.clone()), count);
+        }
+    }
+
+    (dag, edge_costs)
+}
+
+fn path_contains(dag: &HashMap<String, Vec<String>>, current: String, lookup: &str) -> bool {
+    if current == *lookup {
+        return true;
+    }
+
+    let adj_list = dag.get(&current).unwrap();
+
+    if adj_list.is_empty() {
+        return false;
+    }
+
+    adj_list
+        .iter()
+        .any(|vertex| path_contains(dag, vertex.to_string(), lookup))
+}
+
+fn solve(lines: Vec<String>) -> usize {
+    let (bag_dag, _edge_costs) = build_dag(lines);
+
+    bag_dag
+        .iter()
+        .filter(|(vertex, _)| {
+            *vertex != "shiny gold"
+                && path_contains(&bag_dag, vertex.to_string(), &"shiny gold".to_string())
+        })
+        .count()
+}
+
+fn main() {
+    let lines = utils::read_lines();
+    let part1 = solve(lines);
+    println!("Part 1: {}", part1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solve_works() {
+        assert_eq!(
+            solve(
+                vec![
+                    "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                    "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                    "bright white bags contain 1 shiny gold bag.",
+                    "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                    "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                    "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                    "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                    "faded blue bags contain no other bags.",
+                    "dotted black bags contain no other bags.",
+                ]
+                .into_iter()
+                .map(|s| String::from(s))
+                .collect()
+            ),
+            4
+        );
+    }
+}
