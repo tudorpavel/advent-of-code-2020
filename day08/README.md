125840b5a426a35be9b927bb43552af091c93aa9
Tudor Pavel <tudorv.pavel@gmail.com>
2020-12-08 08:26:16 +0200

    Day 8 - Part 2

--- day08/README.md
+++ day08/README.md
@@ -5,3 +5,7 @@ https://adventofcode.com/2020/day/8
 ## Part 1
 
 Should be straightforward to execute the instructions in a while loop and break when visiting the same instruction twice. Seems like a good opportunity to use [Pattern Syntax](https://doc.rust-lang.org/book/ch18-03-pattern-syntax.html) for switching based on operation name.
+
+## Part 2
+
+Okay so we need to extract the existing while loop in a procedure which can report back success/failure and the last value of the `accumulator`. Other than that we need to generate all possible combinations of instructions obtained by switching exactly one `nop` to a `jmp` or vice-versa.
--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,23 +1,13 @@
 use std::collections::HashSet;
 
-fn solve(lines: Vec<String>) -> i32 {
-    let instructions: Vec<(&str, i32)> = lines
-        .iter()
-        .map(|l| {
-            let parts: Vec<&str> = l.split(" ").collect();
-            let operation = parts[0];
-            let argument = parts[1].parse().unwrap();
-
-            (operation, argument)
-        })
-        .collect();
+fn run_program(instructions: &[(&str, i32)]) -> Result<i32, i32> {
     let mut accumulator = 0;
     let mut current = 0;
     let mut visited = HashSet::new();
 
     while current < instructions.len() {
         if visited.contains(&current) {
-            break;
+            return Err(accumulator);
         }
 
         visited.insert(current);
@@ -33,13 +23,58 @@ fn solve(lines: Vec<String>) -> i32 {
         }
     }
 
-    accumulator
+    Ok(accumulator)
+}
+
+fn swap_operations(instruction: &mut (&str, i32)) {
+    match instruction {
+        ("nop", _) => {
+            instruction.0 = "jmp";
+        }
+        ("jmp", _) => {
+            instruction.0 = "nop";
+        }
+        _ => (),
+    }
+}
+
+fn solve(lines: Vec<String>) -> (i32, i32) {
+    let mut instructions: Vec<(&str, i32)> = lines
+        .iter()
+        .map(|l| {
+            let parts: Vec<&str> = l.split(' ').collect();
+            let operation = parts[0];
+            let argument = parts[1].parse().unwrap();
+
+            (operation, argument)
+        })
+        .collect();
+
+    let part1 = match run_program(&instructions) {
+        Ok(accumulator) => accumulator,
+        Err(accumulator) => accumulator,
+    };
+    let mut part2 = 0;
+
+    for i in 0..instructions.len() {
+        swap_operations(&mut instructions[i]);
+
+        if let Ok(accumulator) = run_program(&instructions) {
+            part2 = accumulator;
+            break;
+        }
+
+        swap_operations(&mut instructions[i]);
+    }
+
+    (part1, part2)
 }
 
 fn main() {
     let lines = utils::read_lines();
-    let part1 = solve(lines);
+    let (part1, part2) = solve(lines);
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 #[cfg(test)]
@@ -58,7 +93,7 @@ mod tests {
                 .map(|s| String::from(s))
                 .collect()
             ),
-            5
+            (5, 8)
         );
     }
 }
