653b73863b521ab3fd3c16a29f7c640e310fac87
Tudor Pavel <tudorv.pavel@gmail.com>
2020-12-12 14:19:33 +0200

    Day 11 - Part 2

--- day11/README.md
+++ day11/README.md
@@ -5,3 +5,9 @@ https://adventofcode.com/2020/day/11
 ## Part 1
 
 Should be relatively straightforward to store the layout as a bidimensional vector of chars and have a function which produces next step in the simulation. I feel the code turned out quite awkward because of my ignorance related to Rust data structures and references.
+
+## Part 2
+
+Okay to find first visible seat we need a recursive function that takes a direction tuple such as `(-1, -1)` which is repeatedly applied to coordinates `(i, j)` at recursion step until a seat is reached or the edge of the layout is reached.
+
+Using this we can count the number of occupied seats with the new rule. We can try to reuse some of the logic from Part1 for the common parts.
--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,4 +1,6 @@
-fn adjacent_occupancy_count(layout: &Vec<Vec<char>>, i: usize, j: usize) -> usize {
+type Layout = Vec<Vec<char>>;
+
+fn adjacent_occupancy_count(layout: &Layout, i: usize, j: usize) -> usize {
     let i = i as i32;
     let j = j as i32;
 
@@ -21,11 +23,53 @@ fn adjacent_occupancy_count(layout: &Vec<Vec<char>>, i: usize, j: usize) -> usiz
     .count()
 }
 
-fn total_occupancy_count(layout: &Vec<Vec<char>>) -> usize {
+fn find_occupied(layout: &Layout, i: i32, j: i32, (di, dj): (i32, i32)) -> usize {
+    let i = i + di;
+    let j = j + dj;
+
+    // out of bounds check
+    if !(0..layout.len() as i32).contains(&i) || !(0..layout[0].len() as i32).contains(&j) {
+        return 0;
+    }
+
+    // stop recursion at first seat
+    match layout[i as usize][j as usize] {
+        'L' => {
+            return 0;
+        }
+        '#' => {
+            return 1;
+        }
+        _ => (),
+    }
+
+    find_occupied(layout, i, j, (di, dj))
+}
+
+fn visible_occupancy_count(layout: &Layout, i: usize, j: usize) -> usize {
+    let i = i as i32;
+    let j = j as i32;
+
+    vec![
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ]
+    .iter()
+    .map(|dir| find_occupied(layout, i, j, *dir))
+    .sum()
+}
+
+fn total_occupancy_count(layout: &Layout) -> usize {
     layout.iter().flatten().filter(|c| **c == '#').count()
 }
 
-fn simulation_next(layout: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn part1_simulation_next(layout: &Layout) -> Layout {
     let mut result = layout.to_owned();
 
     for i in 0..layout.len() {
@@ -41,8 +85,24 @@ fn simulation_next(layout: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     result
 }
 
-fn solve(lines: Vec<String>) -> usize {
-    let mut layout: Vec<Vec<char>> = lines.iter().map(|l| l.chars().collect()).collect();
+fn part2_simulation_next(layout: &Layout) -> Layout {
+    let mut result = layout.to_owned();
+
+    for i in 0..layout.len() {
+        for j in 0..layout[0].len() {
+            if layout[i][j] == 'L' && visible_occupancy_count(&layout, i, j) == 0 {
+                result[i][j] = '#';
+            } else if layout[i][j] == '#' && visible_occupancy_count(&layout, i, j) >= 5 {
+                result[i][j] = 'L';
+            }
+        }
+    }
+
+    result
+}
+
+fn run_simulation(lines: &[String], simulation_next: &dyn Fn(&Layout) -> Layout) -> usize {
+    let mut layout: Layout = lines.iter().map(|l| l.chars().collect()).collect();
 
     loop {
         let new_layout = simulation_next(&layout);
@@ -57,10 +117,18 @@ fn solve(lines: Vec<String>) -> usize {
     total_occupancy_count(&layout)
 }
 
+fn solve(lines: Vec<String>) -> (usize, usize) {
+    (
+        run_simulation(&lines, &part1_simulation_next),
+        run_simulation(&lines, &part2_simulation_next),
+    )
+}
+
 fn main() {
     let lines = utils::read_lines();
-    let part1 = solve(lines);
+    let (part1, part2) = solve(lines);
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 #[cfg(test)]
@@ -87,7 +155,7 @@ mod tests {
                 .map(String::from)
                 .collect()
             ),
-            37
+            (37, 26)
         );
     }
 }
