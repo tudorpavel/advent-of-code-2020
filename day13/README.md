bdb1b99e856f9ed7b5ef1886873583f6c4266f6a
Tudor Pavel <tudorv.pavel@gmail.com>
2020-12-13 14:42:22 +0200

    Day 13 - Part 2

--- day13/README.md
+++ day13/README.md
@@ -5,3 +5,49 @@ https://adventofcode.com/2020/day/13
 ## Part 1
 
 Seems like we have to get the remainder of dividing the given earliest departure timestamp with each of the bus IDs and use it to get the minimum difference from our departure timestamp to the next bus departure.
+
+## Part 2
+
+The following brute-force solution ran for longer than 1 minute on my input, so I guess we should figure out some math solution.
+
+```rust
+let mut current_timestamp: u64 = 0;
+
+loop {
+    let is_solution = primes_with_offset
+        .iter()
+        .all(|(prime, offset)| (current_timestamp + *offset as u64) % prime == 0);
+
+    if is_solution {
+        return current_timestamp;
+    }
+
+    current_timestamp += primes_with_offset[0].0;
+}
+```
+
+If we needed to find a timestamp when all buses leave at the same time, we could use [Least common multiple](https://en.wikipedia.org/wiki/Least_common_multiple) which in our case is the product of all bus IDs because they are all prime numbers. But figuring in the offsets doesn't seem trivial...
+
+We might be able to adapt this [LCM simple algorithm](https://en.wikipedia.org/wiki/Least_common_multiple#Using_a_simple_algorithm) to our needs and find the solution more quickly than a brute force search. Update: Still way too slow.
+
+## Part 2 - one day later
+
+Okay, so I woke up the second day with the solution stuck in my head already. We can still use Least common multiple (LCM) to figure out how often a pattern repeats itself. For example with `3,5` first solution is 9 and then it repeats itself every 15 steps because LCM of 3 and 5 is 15:
+
+```
+T  | 3 | 5
+0  | D | D
+...
+8  | - | -
+9  | D | -
+10 | - | D
+11 | - | -
+...
+24 | D | -
+25 | - | D
+...
+39 | D | -
+40 | - | D
+```
+
+We can apply this for each prime in our input and start looking starting from previous solution with increment of LCM of previous primes. The solution at each step is based on the previous solution, so we can narrow down the search space for each prime by a lot.
--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,4 +1,4 @@
-fn solve(lines: Vec<String>) -> usize {
+fn part1(lines: &[String]) -> usize {
     let earliest_timestamp: usize = lines[0].parse().unwrap();
 
     let (bus_id, min_wait) = lines[1]
@@ -12,10 +12,40 @@ fn solve(lines: Vec<String>) -> usize {
     bus_id * min_wait
 }
 
+fn part2(lines: &[String]) -> u64 {
+    // Convert "17,x,13,19" to [17, 0, 13, 19]
+    let bus_ids: Vec<u64> = lines[1]
+        .split(',')
+        .map(|s| s.parse::<u64>().unwrap_or(0))
+        .collect();
+
+    let mut current_solution = 0;
+    let mut step_size: u64 = 1;
+
+    // Insight here is that each previously found pattern
+    // repeats itself every Least common multiple (LCM) steps
+    // and LCM of primes is their product
+    for (offset, bus_id) in bus_ids.iter().enumerate() {
+        if *bus_id == 0 {
+            continue;
+        }
+
+        for timestamp in (current_solution..u64::MAX).step_by(step_size as usize) {
+            if (timestamp + offset as u64) % bus_id == 0 {
+                current_solution = timestamp;
+                step_size *= bus_id;
+                break;
+            }
+        }
+    }
+
+    current_solution
+}
+
 fn main() {
     let lines = utils::read_lines();
-    let part1 = solve(lines);
-    println!("Part 1: {}", part1);
+    println!("Part 1: {}", part1(&lines));
+    println!("Part 2: {}", part2(&lines));
 }
 
 #[cfg(test)]
@@ -23,15 +53,34 @@ mod tests {
     use super::*;
 
     #[test]
-    fn solve_works() {
-        assert_eq!(
-            solve(
-                vec!["939", "7,13,x,x,59,x,31,19"]
-                    .into_iter()
-                    .map(String::from)
-                    .collect()
-            ),
-            295
-        );
+    fn part1_works() {
+        let input: Vec<String> = vec!["939".to_string(), "7,13,x,x,59,x,31,19".to_string()];
+
+        assert_eq!(part1(&input), 295);
+    }
+
+    #[test]
+    fn part2_works_with_initial_example() {
+        let input: Vec<String> = vec!["0".to_string(), "7,13,x,x,59,x,31,19".to_string()];
+
+        assert_eq!(part2(&input), 1068781);
+    }
+
+    #[test]
+    fn part2_works_with_other_examples() {
+        let input: Vec<String> = vec!["0".to_string(), "17,x,13,19".to_string()];
+        assert_eq!(part2(&input), 3417);
+
+        let input: Vec<String> = vec!["0".to_string(), "67,7,59,61".to_string()];
+        assert_eq!(part2(&input), 754018);
+
+        let input: Vec<String> = vec!["0".to_string(), "67,x,7,59,61".to_string()];
+        assert_eq!(part2(&input), 779210);
+
+        let input: Vec<String> = vec!["0".to_string(), "67,7,x,59,61".to_string()];
+        assert_eq!(part2(&input), 1261476);
+
+        let input: Vec<String> = vec!["0".to_string(), "1789,37,47,1889".to_string()];
+        assert_eq!(part2(&input), 1202161486);
     }
 }
