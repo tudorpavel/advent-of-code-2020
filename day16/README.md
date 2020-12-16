7d9db3f5537b8c88d5d9fafc7001c1fe0c0ac024
Tudor Pavel <tudorv.pavel@gmail.com>
2020-12-16 12:53:36 +0200

    Day 16 - Part 2

--- day16/README.md
+++ day16/README.md
@@ -7,3 +7,46 @@ https://adventofcode.com/2020/day/16
 I'm sure the field names will be needed for Part 2, but I will create the minimum necessary data structures just for Part 1. We need to have a collection of all the Ranges from all the rules and a collection of all numbers from all the nearby tickets and then filter the list of numbers based on wether they are part of any of the ranges or not.
 
 It should be possible to express all of this using functional pipelines, let's see how it goes.
+
+## Part 2
+
+Okay so now that we know the entire problem space we can create some data structures to keep things nice and tidy. We need a `Ticket` which can be a type alias for a `Vec<usize>` and we need a `Field` which is a struct something like so:
+
+```rust
+struct Field {
+    name: String,
+    range1: RangeInclusive<usize>,
+    range2: RangeInclusive<usize>,
+}
+```
+
+As for the algorithm of finding which Ticket position corresponds to which field, we have to be careful for overlapping ranges. I assume it's possible some Fields are valid for multiple Ticket positions and then we would need some backtracking to match Fields to Ticket positions, but we can check this assumption. Checked and it's true, multiple fields can match for each Ticket position.
+
+In fact, sorting by the number of matched fields we get this nice sequence:
+
+```rust
+[
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+]
+```
+
+Which means we can assign each Ticket position a Field in this order, always choosing out of the valid Fields the only Field that wasn't previously picked. No need for backtracking this time. :)
--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,5 +1,55 @@
 use std::ops::RangeInclusive;
 
+type Ticket = Vec<usize>;
+
+trait TicketMethods {
+    fn build(line: &String) -> Ticket;
+}
+
+impl TicketMethods for Ticket {
+    fn build(line: &String) -> Ticket {
+        line.split(',').map(|s| s.parse().unwrap()).collect()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Field {
+    name: String,
+    range1: RangeInclusive<usize>,
+    range2: RangeInclusive<usize>,
+}
+
+impl Field {
+    fn build(line: &String) -> Field {
+        let rule: Vec<&str> = line.split(": ").collect();
+        let field_ranges: Vec<&str> = rule[1].split(" or ").collect();
+
+        let nums: Vec<&str> = field_ranges[0].split('-').collect();
+        let range1 = nums[0].parse().unwrap()..=nums[1].parse().unwrap();
+
+        let nums: Vec<&str> = field_ranges[1].split('-').collect();
+        let range2 = nums[0].parse().unwrap()..=nums[1].parse().unwrap();
+
+        Field {
+            name: rule[0].to_string(),
+            range1,
+            range2,
+        }
+    }
+
+    fn ticket_is_valid(&self, ticket: &Ticket) -> bool {
+        ticket
+            .iter()
+            .all(|value| self.range1.contains(value) || self.range2.contains(value))
+    }
+
+    fn tickets_valid_at_offset(&self, tickets: &[Ticket], offset: usize) -> bool {
+        tickets.iter().all(|ticket| {
+            self.range1.contains(&ticket[offset]) || self.range2.contains(&ticket[offset])
+        })
+    }
+}
+
 fn part1(lines: &[String]) -> usize {
     let groups: Vec<&[String]> = lines.split(|l| l == "").collect();
     let all_ranges: Vec<RangeInclusive<usize>> = groups[0]
@@ -37,9 +87,55 @@ fn part1(lines: &[String]) -> usize {
         .sum()
 }
 
+fn part2(lines: &[String]) -> usize {
+    let groups: Vec<&[String]> = lines.split(|l| l == "").collect();
+    let fields: Vec<Field> = groups[0].iter().map(Field::build).collect();
+    let my_ticket = Ticket::build(&groups[1][1]);
+    let nearby_tickets: Vec<Ticket> = groups[2][1..groups[2].len()]
+        .iter()
+        .map(Ticket::build)
+        .filter(|ticket| fields.iter().any(|f| f.ticket_is_valid(&ticket)))
+        .collect();
+
+    let mut valid_fields_for_offsets: Vec<(usize, Vec<&Field>)> = my_ticket
+        .iter()
+        .enumerate()
+        .map(|(offset, _)| {
+            (
+                offset,
+                fields
+                    .iter()
+                    .filter(|field| field.tickets_valid_at_offset(&nearby_tickets, offset))
+                    .collect(),
+            )
+        })
+        .collect();
+
+    valid_fields_for_offsets.sort_by(|a, b| a.1.len().cmp(&(b.1.len())));
+
+    let mut used_fields: Vec<&Field> = vec![];
+    let mut part2_result = 1;
+
+    for (offset, valid_fields) in valid_fields_for_offsets {
+        let chosen_field: &Field = valid_fields
+            .iter()
+            .find(|&&f| !used_fields.contains(&f))
+            .unwrap();
+
+        used_fields.push(chosen_field);
+
+        if chosen_field.name.starts_with("departure") {
+            part2_result *= my_ticket[offset];
+        }
+    }
+
+    part2_result
+}
+
 fn main() {
     let lines = utils::read_lines();
     println!("Part 1: {}", part1(&lines));
+    println!("Part 2: {}", part2(&lines));
 }
 
 #[cfg(test)]
@@ -67,5 +163,28 @@ mod tests {
         .collect();
 
         assert_eq!(part1(&lines), 71);
+    }
+
+    #[test]
+    fn part2_works() {
+        let lines: Vec<String> = vec![
+            "departure station: 0-1 or 4-19",
+            "departure date: 0-5 or 8-19",
+            "seat: 0-13 or 16-19",
+            "",
+            "your ticket:",
+            "11,12,13",
+            "",
+            "nearby tickets:",
+            "3,9,18",
+            "15,1,5",
+            "5,14,9",
+            "500,14,9",
+        ]
+        .into_iter()
+        .map(String::from)
+        .collect();
+
+        assert_eq!(part2(&lines), 132);
     }
 }
