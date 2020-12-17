a995e4119aec4658e3c056af6079bd85631181d3
Tudor Pavel <tudorv.pavel@gmail.com>
2020-12-17 09:02:19 +0200

    Day 17 - Part 2

--- day17/README.md
+++ day17/README.md
@@ -5,3 +5,9 @@ https://adventofcode.com/2020/day/17
 ## Part 1
 
 We can use a 50x50x50 cube defined as 3 nested vectors of booleans and initialize a slice somewhere in the middle. If at any point we go out of bounds, we need a bigger cube. Once in place, we iterate with 3 nested loops all cells inside the cube and determine if each cell lives, dies or is born again.
+
+## Part 2
+
+Okay then, we can duplicate most of the logic and use 4 dimensions. Introducing separate modules for part1 and part2 can help separate functions better. Also, after some trial and error, it seems we can tighten the cube size to 27 to keep things fast.
+
+Runtime with a release build is about 480ms for both parts.
--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,113 +1,230 @@
-const DELTA_NEIGHBORS: [(i32, i32, i32); 26] = [
-    (-1, -1, -1),
-    (-1, -1, 0),
-    (-1, -1, 1),
-    (-1, 0, -1),
-    (-1, 0, 0),
-    (-1, 0, 1),
-    (-1, 1, -1),
-    (-1, 1, 0),
-    (-1, 1, 1),
-    (0, -1, -1),
-    (0, -1, 0),
-    (0, -1, 1),
-    (0, 0, -1),
-    (0, 0, 1),
-    (0, 1, -1),
-    (0, 1, 0),
-    (0, 1, 1),
-    (1, -1, -1),
-    (1, -1, 0),
-    (1, -1, 1),
-    (1, 0, -1),
-    (1, 0, 0),
-    (1, 0, 1),
-    (1, 1, -1),
-    (1, 1, 0),
-    (1, 1, 1),
-];
-const CUBE_SIZE: usize = 50;
-const MID: usize = CUBE_SIZE / 2;
+mod common {
+    pub const CUBE_SIZE: usize = 27;
+    pub const MID: usize = CUBE_SIZE / 2;
 
-type Cube = Vec<Vec<Vec<bool>>>;
+    pub type Cube = Vec<Vec<Vec<bool>>>;
+}
 
-fn step_simulation(cube: &Cube) -> Cube {
-    let mut new_cube = cube.to_owned();
+mod part1 {
+    use crate::common::Cube;
+    use crate::common::CUBE_SIZE;
+    use crate::common::MID;
 
-    for i in 1..CUBE_SIZE - 1 {
-        for j in 1..CUBE_SIZE - 1 {
-            for k in 1..CUBE_SIZE - 1 {
-                let alive_neighbor_count = DELTA_NEIGHBORS
-                    .iter()
-                    .filter(|(di, dj, dk)| {
-                        let ii = (i as i32 + di) as usize;
-                        let jj = (j as i32 + dj) as usize;
-                        let kk = (k as i32 + dk) as usize;
+    const DELTA_NEIGHBORS: [(i32, i32, i32); 26] = [
+        (-1, -1, -1),
+        (-1, -1, 0),
+        (-1, -1, 1),
+        (-1, 0, -1),
+        (-1, 0, 0),
+        (-1, 0, 1),
+        (-1, 1, -1),
+        (-1, 1, 0),
+        (-1, 1, 1),
+        (0, -1, -1),
+        (0, -1, 0),
+        (0, -1, 1),
+        (0, 0, -1),
+        (0, 0, 1),
+        (0, 1, -1),
+        (0, 1, 0),
+        (0, 1, 1),
+        (1, -1, -1),
+        (1, -1, 0),
+        (1, -1, 1),
+        (1, 0, -1),
+        (1, 0, 0),
+        (1, 0, 1),
+        (1, 1, -1),
+        (1, 1, 0),
+        (1, 1, 1),
+    ];
 
-                        cube[ii][jj][kk]
-                    })
-                    .count();
+    fn step_simulation(cube: &Cube) -> Cube {
+        let mut new_cube = cube.to_owned();
 
-                if cube[i][j][k] && !(2..=3).contains(&alive_neighbor_count) {
-                    new_cube[i][j][k] = false;
-                } else if !cube[i][j][k] && alive_neighbor_count == 3 {
-                    new_cube[i][j][k] = true;
+        for i in 1..CUBE_SIZE - 1 {
+            for j in 1..CUBE_SIZE - 1 {
+                for k in 1..CUBE_SIZE - 1 {
+                    let alive_neighbor_count = DELTA_NEIGHBORS
+                        .iter()
+                        .filter(|(di, dj, dk)| {
+                            let ii = (i as i32 + di) as usize;
+                            let jj = (j as i32 + dj) as usize;
+                            let kk = (k as i32 + dk) as usize;
+
+                            cube[ii][jj][kk]
+                        })
+                        .count();
+
+                    if cube[i][j][k] && !(2..=3).contains(&alive_neighbor_count) {
+                        new_cube[i][j][k] = false;
+                    } else if !cube[i][j][k] && alive_neighbor_count == 3 {
+                        new_cube[i][j][k] = true;
+                    }
                 }
             }
         }
+
+        new_cube
     }
 
-    new_cube
-}
+    fn alive_cell_count(cube: &Cube) -> u32 {
+        let mut count = 0;
 
-fn alive_cell_count(cube: &Cube) -> u32 {
-    let mut count = 0;
-
-    for i in 0..CUBE_SIZE {
-        for j in 0..CUBE_SIZE {
-            for k in 0..CUBE_SIZE {
-                if cube[i][j][k] {
-                    count += 1;
+        for i in 0..CUBE_SIZE {
+            for j in 0..CUBE_SIZE {
+                for k in 0..CUBE_SIZE {
+                    if cube[i][j][k] {
+                        count += 1;
+                    }
                 }
             }
         }
+
+        count
     }
 
-    count
+    pub fn solve(lines: &[String]) -> u32 {
+        let mut cube: Cube = vec![vec![vec![false; CUBE_SIZE]; CUBE_SIZE]; CUBE_SIZE];
+
+        for (i, line) in lines.iter().enumerate() {
+            for (j, cell) in line.chars().enumerate() {
+                cube[MID + i][MID + j][MID] = cell == '#';
+            }
+        }
+
+        for _ in 0..6 {
+            cube = step_simulation(&cube);
+        }
+
+        alive_cell_count(&cube)
+    }
 }
 
-fn solve(lines: &[String]) -> u32 {
-    let mut cube: Cube = vec![vec![vec![false; CUBE_SIZE]; CUBE_SIZE]; CUBE_SIZE];
+mod part2 {
+    use crate::common::Cube;
+    use crate::common::CUBE_SIZE;
+    use crate::common::MID;
 
-    for (i, line) in lines.iter().enumerate() {
-        for (j, cell) in line.chars().enumerate() {
-            cube[MID + i][MID + j][MID] = cell == '#';
+    type HyperCube = Vec<Cube>;
+
+    fn build_delta_neighors() -> Vec<(i32, i32, i32, i32)> {
+        let mut result = vec![];
+
+        for i in -1..=1 {
+            for j in -1..=1 {
+                for k in -1..=1 {
+                    for l in -1..=1 {
+                        if (i, j, k, l) == (0, 0, 0, 0) {
+                            continue;
+                        } else {
+                            result.push((i, j, k, l));
+                        }
+                    }
+                }
+            }
         }
+
+        result
     }
 
-    for _ in 0..6 {
-        cube = step_simulation(&cube);
+    fn step_simulation(
+        hyper_cube: &HyperCube,
+        delta_neighbors: &Vec<(i32, i32, i32, i32)>,
+    ) -> HyperCube {
+        let mut new_hyper_cube = hyper_cube.to_owned();
+
+        for i in 1..CUBE_SIZE - 1 {
+            for j in 1..CUBE_SIZE - 1 {
+                for k in 1..CUBE_SIZE - 1 {
+                    for l in 1..CUBE_SIZE - 1 {
+                        let alive_neighbor_count = delta_neighbors
+                            .iter()
+                            .filter(|(di, dj, dk, dl)| {
+                                let ii = (i as i32 + di) as usize;
+                                let jj = (j as i32 + dj) as usize;
+                                let kk = (k as i32 + dk) as usize;
+                                let ll = (l as i32 + dl) as usize;
+
+                                hyper_cube[ii][jj][kk][ll]
+                            })
+                            .count();
+
+                        if hyper_cube[i][j][k][l] && !(2..=3).contains(&alive_neighbor_count) {
+                            new_hyper_cube[i][j][k][l] = false;
+                        } else if !hyper_cube[i][j][k][l] && alive_neighbor_count == 3 {
+                            new_hyper_cube[i][j][k][l] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        new_hyper_cube
     }
 
-    alive_cell_count(&cube)
+    fn alive_cell_count(hyper_cube: &HyperCube) -> u32 {
+        let mut count = 0;
+
+        for i in 0..CUBE_SIZE {
+            for j in 0..CUBE_SIZE {
+                for k in 0..CUBE_SIZE {
+                    for l in 0..CUBE_SIZE {
+                        if hyper_cube[i][j][k][l] {
+                            count += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        count
+    }
+
+    pub fn solve(lines: &[String]) -> u32 {
+        let mut hyper_cube: HyperCube =
+            vec![vec![vec![vec![false; CUBE_SIZE]; CUBE_SIZE]; CUBE_SIZE]; CUBE_SIZE];
+        let delta_neighbors = build_delta_neighors();
+
+        for (i, line) in lines.iter().enumerate() {
+            for (j, cell) in line.chars().enumerate() {
+                hyper_cube[MID + i][MID + j][MID][MID] = cell == '#';
+            }
+        }
+
+        for _ in 0..6 {
+            hyper_cube = step_simulation(&hyper_cube, &delta_neighbors);
+        }
+
+        alive_cell_count(&hyper_cube)
+    }
 }
 
 fn main() {
     let lines = utils::read_lines();
-    println!("Part 1: {}", solve(&lines));
+    println!("Part 1: {}", crate::part1::solve(&lines));
+    println!("Part 2: {}", crate::part2::solve(&lines));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    fn solve_works() {
+    fn part1_works() {
         let lines: Vec<String> = vec![".#.", "..#", "###"]
             .into_iter()
             .map(String::from)
             .collect();
 
-        assert_eq!(solve(&lines), 112);
+        assert_eq!(crate::part1::solve(&lines), 112);
+    }
+
+    #[test]
+    fn part2_works() {
+        let lines: Vec<String> = vec![".#.", "..#", "###"]
+            .into_iter()
+            .map(String::from)
+            .collect();
+
+        assert_eq!(crate::part2::solve(&lines), 848);
     }
 }
