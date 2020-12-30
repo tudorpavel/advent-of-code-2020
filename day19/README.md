1a600a1c769d6d72cdeacbd065452d84b7fa6f2b
Tudor Pavel <tudorv.pavel@gmail.com>
2020-12-30 22:52:46 +0200

    Day 19 - Part 2

--- day19/README.md
+++ day19/README.md
@@ -5,3 +5,28 @@ https://adventofcode.com/2020/day/19
 ## Part 1
 
 There is a way to construct a binary tree such that it codifies all possibilities by marking a left branch as `a` and a right branch as `b` and this should make verifying a given message easy. However, it might not be so easy to construct the tree from the given rules, especially since I'm a Rust newbie that needs to implement a data structure with pointers. Let's see how it goes...
+
+## Part 2
+
+So... I went down a huge rabbit hole on this one. I thought I could accomodate the recursive rules in my binary tree solution using some reference cycles but I had to give up. The way I implemented building the trees is cloning subproblem trees and I couldn't manage to reuse subproblems by just having pointers to them.
+
+After many hours in many different days I looked closer at the input and realized rule 0 is composed of just rules 8 and 11 which are the ones that change for Part 2 and become recursive:
+
+```
+0: 8 11
+8: 42 | 42 8
+11: 42 31 | 42 11 31
+```
+
+What this means is that we can split each message in chunks and try different sequences of rules 42 and 31 to validate a message. Quite conveniently the subproblem size is 8 for both rule 42 and 31 so we can split the message in chunks of 8 and we have a finite set of combinations we can try depending on the number of chunks.
+
+For example, if the message is split in 9 chunks we have to try the following sequences:
+
+```
+42 42 42 42 42 42 42 42 31
+42 42 42 42 42 42 42 31 31
+42 42 42 42 42 42 31 31 31
+42 42 42 42 42 31 31 31 31
+```
+
+The message is valid if at least one sequence of rules matches the message chunks one by one.
--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,6 +1,9 @@
+use std::collections::HashSet;
+use std::rc::Rc;
+
 #[derive(Clone)]
 enum Tree {
-    Node(Box<Tree>, Box<Tree>),
+    Node(Rc<Tree>, Rc<Tree>),
     Solution,
     Nil,
 }
@@ -10,8 +13,8 @@ use Tree::{Nil, Node, Solution};
 impl Tree {
     fn build(rules: &[&str], rule_id: usize) -> Tree {
         match rules[rule_id] {
-            "\"a\"" => Node(Box::new(Solution), Box::new(Nil)),
-            "\"b\"" => Node(Box::new(Nil), Box::new(Solution)),
+            "\"a\"" => Node(Rc::new(Solution), Rc::new(Nil)),
+            "\"b\"" => Node(Rc::new(Nil), Rc::new(Solution)),
             composed_rule => composed_rule
                 .split(" | ")
                 .map(|append_rules| {
@@ -38,12 +41,32 @@ impl Tree {
         }
     }
 
+    fn solution_depths(&self, depth: usize) -> HashSet<usize> {
+        match self {
+            Solution => {
+                let mut result = HashSet::new();
+                result.insert(depth);
+                result
+            }
+            Node(left, right) => {
+                let union: HashSet<_> = left
+                    .solution_depths(depth + 1)
+                    .union(&right.solution_depths(depth + 1))
+                    .copied()
+                    .collect();
+
+                union
+            }
+            _ => HashSet::new(),
+        }
+    }
+
     fn append_solution(&self, other: &Tree) -> Tree {
         match self {
             Solution => other.clone(),
             Node(left, right) => Node(
-                Box::new(left.append_solution(&other)),
-                Box::new(right.append_solution(&other)),
+                Rc::new(left.append_solution(&other)),
+                Rc::new(right.append_solution(&other)),
             ),
             Nil => Nil,
         }
@@ -53,16 +76,16 @@ impl Tree {
         match (self, other) {
             (Nil, _) => other.clone(),
             (_, Nil) => self.clone(),
-            (Node(l1, r1), Node(l2, r2)) => Node(Box::new(l1.merge(l2)), Box::new(r1.merge(r2))),
+            (Node(l1, r1), Node(l2, r2)) => Node(Rc::new(l1.merge(l2)), Rc::new(r1.merge(r2))),
             _ => Nil,
         }
     }
 }
 
-fn solve(lines: &[String]) -> usize {
+fn parse_input(lines: &[String]) -> (Vec<&str>, &[String]) {
     let groups: Vec<&[String]> = lines.split(|l| l == "").collect();
     let rules = groups[0];
-    let rules: Vec<&str> = rules.iter().fold(vec![""; rules.len()], |mut acc, rule| {
+    let rules: Vec<&str> = rules.iter().fold(vec![""; 135], |mut acc, rule| {
         let parts: Vec<&str> = rule.split(": ").collect();
         let rule_id: usize = parts[0].parse().unwrap();
 
@@ -71,17 +94,56 @@ fn solve(lines: &[String]) -> usize {
         acc
     });
 
+    (rules, groups[1])
+}
+
+fn part1(lines: &[String]) -> usize {
+    let (rules, messages) = parse_input(lines);
+
     let rule_tree = Tree::build(&rules, 0);
 
-    groups[1]
+    messages
         .iter()
         .filter(|message| rule_tree.is_valid(message))
         .count()
 }
 
+fn part2(lines: &[String]) -> usize {
+    let (rules, messages) = parse_input(lines);
+
+    let tree42 = Tree::build(&rules, 42);
+    let tree31 = Tree::build(&rules, 31);
+    let depth = *tree42.solution_depths(0).iter().next().unwrap();
+
+    messages
+        .iter()
+        .filter(|message| {
+            let chunks: Vec<String> = message
+                .as_bytes()
+                .chunks(depth)
+                .map(|chunk| String::from_utf8(chunk.to_vec()).unwrap())
+                .collect();
+
+            (1..=((chunks.len() - 1) / 2))
+                .map(|count31| {
+                    let mut combination = vec![&tree42; chunks.len() - count31];
+                    combination.append(&mut vec![&tree31; count31]);
+                    combination
+                })
+                .any(|trees| {
+                    trees
+                        .iter()
+                        .zip(chunks.iter())
+                        .all(|(tree, chunk)| tree.is_valid(chunk))
+                })
+        })
+        .count()
+}
+
 fn main() {
     let lines = utils::read_lines();
-    println!("Part 1: {}", solve(&lines));
+    println!("Part 1: {}", part1(&lines));
+    println!("Part 2: {}", part2(&lines));
 }
 
 #[cfg(test)]
@@ -89,7 +151,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn solve_works() {
+    fn part1_works_with_small_example() {
         let lines: Vec<String> = vec![
             "0: 4 1 5",
             "1: 2 3 | 3 2",
@@ -108,14 +170,79 @@ mod tests {
         .map(String::from)
         .collect();
 
-        assert_eq!(solve(&lines), 2);
+        assert_eq!(part1(&lines), 2);
+    }
+
+    fn large_example() -> Vec<String> {
+        vec![
+            "42: 9 14 | 10 1",
+            "9: 14 27 | 1 26",
+            "10: 23 14 | 28 1",
+            "1: \"a\"",
+            "11: 42 31",
+            "5: 1 14 | 15 1",
+            "19: 14 1 | 14 14",
+            "12: 24 14 | 19 1",
+            "16: 15 1 | 14 14",
+            "31: 14 17 | 1 13",
+            "6: 14 14 | 1 14",
+            "2: 1 24 | 14 4",
+            "0: 8 11",
+            "13: 14 3 | 1 12",
+            "15: 1 | 14",
+            "17: 14 2 | 1 7",
+            "23: 25 1 | 22 14",
+            "28: 16 1",
+            "4: 1 1",
+            "20: 14 14 | 1 15",
+            "3: 5 14 | 16 1",
+            "27: 1 6 | 14 18",
+            "14: \"b\"",
+            "21: 14 1 | 1 14",
+            "25: 1 1 | 1 14",
+            "22: 14 14",
+            "8: 42",
+            "26: 14 22 | 1 20",
+            "18: 15 15",
+            "7: 14 5 | 1 21",
+            "24: 14 1",
+            "",
+            "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa",
+            "bbabbbbaabaabba",
+            "babbbbaabbbbbabbbbbbaabaaabaaa",
+            "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
+            "bbbbbbbaaaabbbbaaabbabaaa",
+            "bbbababbbbaaaaaaaabbababaaababaabab",
+            "ababaaaaaabaaab",
+            "ababaaaaabbbaba",
+            "baabbaaaabbaaaababbaababb",
+            "abbbbabbbbaaaababbbbbbaaaababb",
+            "aaaaabbaabaaaaababaa",
+            "aaaabbaaaabbaaa",
+            "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
+            "babaaabbbaaabaababbaabababaaab",
+            "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba",
+        ]
+        .into_iter()
+        .map(String::from)
+        .collect()
+    }
+
+    #[test]
+    fn part1_works_with_large_example() {
+        assert_eq!(part1(&large_example()), 3);
+    }
+
+    #[test]
+    fn part2_works_with_large_example() {
+        assert_eq!(part2(&large_example()), 12);
     }
 
     #[test]
     fn is_valid_works() {
         let rule2 = Node(
-            Box::new(Node(Box::new(Solution), Box::new(Nil))),
-            Box::new(Node(Box::new(Nil), Box::new(Solution))),
+            Rc::new(Node(Rc::new(Solution), Rc::new(Nil))),
+            Rc::new(Node(Rc::new(Nil), Rc::new(Solution))),
         );
 
         assert!(rule2.is_valid("aa"));
@@ -124,12 +251,12 @@ mod tests {
     #[test]
     fn append_solution_works() {
         let rule2 = Node(
-            Box::new(Node(Box::new(Solution), Box::new(Nil))),
-            Box::new(Node(Box::new(Nil), Box::new(Solution))),
+            Rc::new(Node(Rc::new(Solution), Rc::new(Nil))),
+            Rc::new(Node(Rc::new(Nil), Rc::new(Solution))),
         );
         let rule3 = Node(
-            Box::new(Node(Box::new(Nil), Box::new(Solution))),
-            Box::new(Node(Box::new(Solution), Box::new(Nil))),
+            Rc::new(Node(Rc::new(Nil), Rc::new(Solution))),
+            Rc::new(Node(Rc::new(Solution), Rc::new(Nil))),
         );
         let append_2_3 = rule2.append_solution(&rule3);
 
@@ -138,8 +265,8 @@ mod tests {
 
     #[test]
     fn merge_works() {
-        let rule4 = Node(Box::new(Solution), Box::new(Nil));
-        let rule5 = Node(Box::new(Nil), Box::new(Solution));
+        let rule4 = Node(Rc::new(Solution), Rc::new(Nil));
+        let rule5 = Node(Rc::new(Nil), Rc::new(Solution));
         let append_4_5 = rule4.append_solution(&rule5);
         let append_5_4 = rule5.append_solution(&rule4);
         let merge_4_5_with_5_4 = append_4_5.merge(&append_5_4);
